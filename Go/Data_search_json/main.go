2a54705c0de0cd931250fa1f53c8a431560f70c4
tee <egor@6693790.ru>
2023-01-08 08:09:34 +0300

    Обновление

--- Go/Data_search_json/main.go
+++ Go/Data_search_json/main.go
@@ -1,11 +1,42 @@
 package main
 
-import "os"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
 
 type Properties struct {
 	Id int `json:"global_id"`
 }
 
+func CheckErr(err error, s string) {
+	if err != nil {
+		str := fmt.Sprintf("%s: %v\n", s, err)
+		panic(str)
+	}
+}
+
 func main() {
-	file, err := os.Open("./data/")
+	file, err := os.Open("./data/data-20190514T0100.json")
+	CheckErr(err, "Ошибка открытия файла")
+	defer func() {
+		err := file.Close()
+		CheckErr(err, "Ошибка закрытия файла")
+	}()
+
+	data, err := io.ReadAll(file)
+	CheckErr(err, "Ошибка чтения файла")
+
+	var decode []Properties
+	err = json.Unmarshal(data, &decode)
+	CheckErr(err, "Ошибка раскодирования файла")
+
+	sum := 0
+	for _, i := range decode {
+		sum += i.Id
+	}
+
+	fmt.Println(sum)
 }
